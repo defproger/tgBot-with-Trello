7212efc126b238aa4012757298e3d097ae6079ad
Default Programmer <defproger@gmail.com>
2024-11-12 17:57:35 +0200

    base with chats prototype

--- db.sql
+++ db.sql
@@ -1,11 +1,24 @@
-create table users
+create table chats
 (
-    telegram_id bigint       not null,
-    username    varchar(50)  null,
-    name        varchar(100) null,
-    trello_id   int          null,
-    constraint users_pk
-        primary key (telegram_id)
+    id      int auto_increment
+        primary key,
+    chat_id varchar(228) null
 );
 
+create table users
+(
+    telegram_id bigint       not null
+        primary key,
+    username    varchar(50)  null,
+    name        varchar(100) null,
+    trello_id   int          null
+);
+
+create table usersInChats
+(
+    id   int auto_increment
+        primary key,
+    uid  bigint null,
+    chat int    null
+);
 
