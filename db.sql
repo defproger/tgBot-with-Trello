5e57229a2f7c20131106a0d80cf94217481d1e8e
Default Programmer <defproger@gmail.com>
2024-11-12 19:14:48 +0200

    modify base

--- db.sql
+++ db.sql
@@ -2,23 +2,20 @@ create table chats
 (
     id      int auto_increment
         primary key,
-    chat_id varchar(228) null
-);
-
-create table users
-(
-    telegram_id bigint       not null
-        primary key,
-    username    varchar(50)  null,
-    name        varchar(100) null,
-    trello_id   int          null
+    chat_id varchar(228) null,
+    constraint chats_pk2
+        unique (chat_id)
 );
 
 create table usersInChats
 (
-    id   int auto_increment
+    id        int auto_increment
         primary key,
-    uid  bigint null,
-    chat int    null
+    uid       bigint       null,
+    chat      int          null,
+    trello_id varchar(100) null,
+    name      varchar(228) null,
+    constraint usersInChats_pk2
+        unique (uid, chat)
 );
 
